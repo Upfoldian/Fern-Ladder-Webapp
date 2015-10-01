21bb884d198a26d086dfc2edcef3a7fea992d67a
Thomas <thomasupfold@gmail.com>
2015-10-01 21:05:38 +1000

    second day

--- ladder.rb
+++ ladder.rb
@@ -13,3 +13,68 @@
 # 		- Naming setups
 # 		- Assigning matches to setups
 # 		- Freeing setups after match report
+
+
+
+class Ladder
+
+
+	attr_reader :activePlayers, :inactivePlayers
+	attr_reader :currentMatches, :completedMatches
+	attr_reader :freeSetups, :usedSetups
+	attr_reader :gameCount
+
+
+	def initialize players, setups, gameCount
+
+		@activePlayers		= players
+		@inactivePlayers	= []
+		@currentMatches 	= []
+		@completedMatches 	= []
+		@freeSetups 		= setups
+		@usedSetups 		= []
+		@ganeCount 			= gameCount
+
+	end
+
+	def startMatch player1. player2, setup
+
+		# Take players out of active player pool
+
+		# Take setup out of active setup pool
+
+		# Add match to currentMatches list
+
+	end
+
+	def matchEnded match
+
+		#Take match out of current matches
+
+		#Add match to completed matches
+
+		#Do elo calcs?
+
+		#Add players back into active pool
+
+		#Add setup back into active pool
+
+	end
+
+	def addSetup name, info, type
+
+		newSetup = Setup.new(name, info, type)
+		setupList.push newSetup
+
+	end
+
+	def addPlayer name
+
+		newPlayer = Player.new(name)
+		activePlayers.push newPlayer
+
+	end
+
+end
+
+		
--- match.rb
+++ match.rb
@@ -1,8 +1,8 @@
 class Match
 
 
-	attr_reader :player1, :player2, :gameCount
-	attr_accessor :player1Score, :player2Score, :winner
+	attr_reader :player1, :player2, :gameCount, :eloShift
+	attr_accessor :player1Score, :player2Score, :winner, :loser
 
 
 	def initialize player1, player2, gameCount
@@ -10,19 +10,36 @@ class Match
 		@player1 		= player1
 		@player2 		= player2
 		@gameCount 		= gameCount
+		@eloShift		= nil
 		@player1Score 	= 0
 		@player2Score 	= 0
 		@winner 		= nil
+		@loser 			= nil
 
 	end
 
 
-	def reportMatch player1Score, player2Score, winner
+	def reportMatch player1Score, player2Score, winner, loser
 
 		@player1Score 	= player1Score
 		@player2Score 	= player2Score
 		@winner		 	= winner
+		@loser 			= loser
 
+
+		eloChange = calculate_elo_change(winner.elo, loser.elo)
+		winner.new_elo eloChange
+		loser.new_elo eloChange
+		@eloShift = eloChange
+
+	end
+
+	def self.calculate_elo_change(winnerElo, loserElo)
+		#Rn = Ro + C * (S - Se)
+		expectedScore = 1.0/(1+10**((loser.elo-winner.elo)/400.0))
+		newRating = ELO_CONST * 1 * (1-expectedScore)
+		#puts newRating
+		return newRating.floor
 	end
 
 end
